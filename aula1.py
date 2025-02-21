a372bc982066760a90576f22dd4f30c5c5778518
lucas <lucas.ivachuka@gmail.com>
2025-02-21 09:30:28 -0300

    jfhjhg

--- aula1.py
+++ aula1.py
@@ -1,7 +1,21 @@
 import random
 
 sorteio = random.randint(1, 10)
-chute = input("digite o seu chute:")
-print(sorteio)
-print(chute)
+print("###jogo da adivinhação###")
+print("adivinhe o numero de 1 a 10")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("tentativa:", tentativa)
+    chute = int(input("digite o seu chute:"))
+    if(chute == sorteio):
+        print ("Parabéns,você acertou!")
+        break
+    elif(chute > sorteio):
+        print("Chute um número maior!")
+    elif (chute < sorteio):
+        print("chute um número maior!")
+    tentativa = tentativa + 1  
+
+
 
