2cc360cfe7efd13bc14194baf94a14971d466d87
lucas <lucas.ivachuka@gmail.com>
2025-02-28 08:54:48 -0300

    Lucas

--- aula1.py
+++ aula1.py
@@ -1,9 +1,26 @@
-import random
+print("1 - Opção 1")
+print("2 - Opção 2")
+print("3 - Opção 3")
+opcao = int(input("Digite a opção desejada:"))
+if (opcao == 1):
+    print("Você escolheu 1")
+    numero_m = 10
+    limite_tentativas = 3
+elif (opcao == 2):
+    numero_m = 20
+    limite_tentativas = 5
+    print("Você escolheu 2")
+elif (opcao == 3):
+    numero_m = 50
+    limite_tentativas = 10
+    print("Você escolheu 3")
+else:
+    print("Opção inválida")
 
-sorteio = random.randint(1, 10)
+import random
+sorteio = random.randint(1, numero_m)
 print("###jogo da adivinhação###")
-print("adivinhe o numero de 1 a 10")
-limite_tentativas = 3
+print("adivinhe o numero de 1 a", numero_m)
 tentativa = 1
 while (limite_tentativas >= tentativa):
     print("tentativa:", tentativa)
@@ -12,10 +29,11 @@ while (limite_tentativas >= tentativa):
         print ("Parabéns,você acertou!")
         break
     elif(chute > sorteio):
-        print("Chute um número maior!")
+        print("Chute um número menor!")
     elif (chute < sorteio):
         print("chute um número maior!")
     tentativa = tentativa + 1  
 
-
+print("O número sorteado era: ##", sorteio, "##")
+print("### FIM DO JOGO ###")
 
